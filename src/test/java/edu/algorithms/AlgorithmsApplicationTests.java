bad0a58313143248d9be3a06d759d2fb9f63635c
Anderson Miranda Bastos <anderson.bastos@resourceit.com>
2020-05-21 11:09:51 -0300

    Initial commit

---
+++ src/test/java/edu/algorithms/AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
